ae5a25494d4561cc58873b4c6592c4ced57c94a2
phil <none@none>
2007-10-22 06:24:35 +0000

    THIS CHANGES IMPORTS NOW NEED import rdfalchemy not rdfAlchemy added setup.py to start egg building
    --HG--
    extra : convert_revision : svn%3A9283dc3d-0829-0410-a9a8-f2d03961dc1a/rdfalchemy/trunk%4058

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='RDFAlchemy',
+    version="0.1",
+    tag_dev=".dev",
+    description="rdflib wrapper",
+    author='Philip Cooper',
+    author_email='philip.cooper@openvest.com',
+    url="http://www.openvest.com/trac/wiki/RDFAlchemy",
+    install_requires=["rdflib>=2.4.0"],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+)
