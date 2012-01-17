f5fe1ace6f85032c7d190107f4459a326d0b1971
Graham Higgins <gjh@bel-epa.com>
2012-01-17 01:07:13 +0000

    Restore BRTR test

--- test/sesame2_test.py
+++ test/sesame2_test.py
@@ -1,22 +1,26 @@
 from rdfalchemy.sparql.sesame2 import SesameGraph
 
-url = 'http://www.openvest.com:8080/openrdf-sesame/repositories/Portfolio/'
-url = 'http://bel-epa.com:8080/openrdf-workbench/repositories/otter/query'
+url = 'http://localhost:8080/bigdata/sparql'
 
 g = SesameGraph(url)
 
-q1 = "select ?s ?p ?o where {?s ?p ?o} limit 10"
+q1 = "select ?s ?p ?o where {?s ?p ?o} limit 100"
 
 responses = {}
 x = set(list(g.query(q1,resultMethod='xml')))
-try:
-	j = set(list(g.query(q1,resultMethod='json')))
-except ValueError:
-	from nose import SkipTest
-	raise SkipTest("It's that JSON problem again")
+j = set(list(g.query(q1,resultMethod='json')))
+b = set(list(g.query(q1,resultMethod='brtr')))
+
+print(len(x))
 
 def sizes_test():
-    assert len(x) == len(j)
+    assert len(b) == len(x) == len(j)
+
+def eq_bx_test():
+    assert b == x
+
+def eq_bj_test():
+    assert b == j
 
 def eq_jx_test():
     assert j == x
